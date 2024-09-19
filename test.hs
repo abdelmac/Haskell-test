de8520492d8958bc5e7229303eafb0774549fb65
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2024-09-19 18:16:00 +0200

    Update test.hs

--- test.hs
+++ test.hs
@@ -2,4 +2,9 @@ map (\x -> x/2) $ filter (\y -> y >= 4 ) 1
 map (\x -> x/2) $ filter (\y -> y >= 4 ) 1
 sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
 sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
 sheesh
