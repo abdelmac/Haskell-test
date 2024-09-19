4708a508a46cf7f7e87c710268d970d866416590
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2024-09-19 18:15:52 +0200

    evz
    zv

--- test.hs
+++ test.hs
@@ -1,1 +1,5 @@
 map (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+map (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheesh
