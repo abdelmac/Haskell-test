7ba22468ca4b2b343c68ceb503bd857a4db3a64f
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2024-09-19 18:04:33 +0200

    sc
    sv

--- test.hs
+++ test.hs
@@ -1,1 +1,25 @@
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+map (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheeshmap (\x -> x/2) $ filter (\y -> y >= 4 ) 1
+sheesh
